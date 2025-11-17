bfeb20c24329a1fa2f35edc790266477e8aafde9
murega14 <tedmurega@gmail.com>
2025-11-17 22:30:56 +0300

    Implement edit and delete functionality for drinks and carwash income entries

--- app/bar/app.py
+++ app/bar/app.py
@@ -54,3 +54,82 @@ def add_drinks():
         logger.error(f"an error occured: {str(e)}", extra={'user_id': get_jwt_identity()})
         return make_response({"success": False, "msg": "internal server error"}, 500)
     
+
+@bar_bp.route('/drinks/<int:drink_id>/edit', methods=['PUT'])
+@jwt_required()
+def edit_drinks(drink_id: int):
+    try:
+        drink = Drink.query.get(drink_id)
+        if not drink:
+            return make_response({'success': False, 'msg': 'drink not found'}, 404)
+        
+        data = request.get_json()
+        
+        try:
+            if 'name' in data:
+                drink.name = data['name']
+                
+            if 'drink_type' in data:
+                if data['drink_type'] in DRINK_TYPES:
+                    drink.drink_type = data['drink_type']
+                else:
+                    return make_response({'success': False, 'msg': f'drink types can only be on of {', '.join(DRINK_TYPES)}'}, 400)
+                
+            if 'stock' in data:
+                drink.stock = int(data['stock'])
+                
+            if 'purchase_price' in data:
+                drink.purchase_price = data['purchase_price']
+            
+            if 'markup' in data:
+                drink.markup = float(data['markup'])
+            
+            if 'volume' in data:
+                if data['volume'] in DRINK_VOLUME:
+                    drink.volume = data['volume']
+                else:
+                    return make_response({'success': False, 'msg': f'volume can only be one of {', '.join(DRINK_VOLUME)}'}, 400)
+                
+            if 'shot_price' in data:
+                drink.shot_price = float(data['shot_price'])
+            
+            if 'shot_quantity' in data:
+                drink.shot_quantity = data['shot_quantity']
+                
+            db.session.commit()
+            logger.info(f"drink {drink_id} has been updated", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': True, 'msg': 'drink details have been updated successfully'}, 200)
+        
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            logger.error(f"database error occured when trying to update drink details: {str(e)}", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': False, 'msg': 'failed to update drink details, please try again'}, 500)
+        
+    except Exception as e:
+        logger.error(f"an error occured when trying to update drink details: {str(e)}", extra={'user_id': get_jwt_identity()})
+        return make_response({'success': False, 'msg': 'internal server error'}, 500)
+
+@bar_bp.route('/drinks/<int:drink_id>/delete', methods=['DELETE'])
+@jwt_required()
+def delete_drink(drink_id: int):
+    try:
+        drink = Drink.query.get(drink_id)
+        if not drink:
+            return make_response({'success': False, 'msg': 'drink does not exist'}, 404)
+        
+        try:
+            db.session.delete(drink)
+            db.session.commit()
+            
+            logger.info(f"drink {drink.name}, {drink.volume} has been deleted", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': True, 'msg': 'drink has been deleted'}, 200)
+        
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            logger.error(f"database error occured when trying to delete drink: {str(e)}", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': False, 'msg': 'failed to delete drink, please try again'}, 500)
+        
+    except Exception as e:
+        logger.error(f"an error occured when trying to delete a drink: {str(e)}", extra={'user_id': get_jwt_identity()})
+        return make_response({'success': False, 'msg': 'internal server error'}, 500)
+                
--- app/carwash/carwash.py
+++ app/carwash/carwash.py
@@ -65,8 +65,98 @@ def add_carwash_income():
         except IntegrityError as e:
             db.session.rollback()
             logger.error(f"unique constraint violation for payment reference number: {str(e)}", extra={'user_id': get_jwt_identity()})
-            return make_response({'success': False, 'msg': 'payment reference number exists'}, 500)
+            return make_response({'success': False, 'msg': 'payment reference number exists'}, 400)
         
     except Exception as e:
         logger.error(f"an error occured trying to record carwash income: {str(e)}", extra={'user_id': get_jwt_identity()})
+        return make_response({'success': False, 'msg': 'internal server error'}, 500)
+    
+@carwash_bp.route('/carwarsh/income/<id:income_id>/edit', methods=['PUT'])
+@jwt_required()
+def edit_carwash_income(income_id: int):
+    try:
+        carwash_income = CarwashIncome.query.get(income_id)
+        if not carwash_income:
+            return make_response({'success': False, 'msg': 'income record not found'}, 404)
+        
+        data = request.get_json()
+        
+        try:
+            if 'customer' in data:
+                carwash_income.customer = data['cutomer']
+            
+            if 'staff_id' in data:
+                staff = Staff.query.filter(and_(
+                    Staff.id == data['staff_id'], staff.department == 'carwash'
+                )).first()
+                if staff:
+                    carwash_income.staff_id = data['staff_id']
+                else:
+                    return make_response({'success': False, 'msg': 'staff not found'}, 404)
+            
+            if 'amount_charged' in data:
+                carwash_income.amount_charged = float(data['amount_charged'])
+            
+            if 'payment_method' in data:
+                if data['payment_method'] in PAYMENT_METHODS:
+                    carwash_income.payment_method = data['payment_method']
+                else:
+                    return make_response({'success': False, 'msg': f'payment method can only be: {', '.join(PAYMENT_METHODS)}'}, 400)
+                
+            if 'payment_reference_number' in data:
+                carwash_income.payment_reference_number = data['reference_number']
+                
+            if 'service' in data:
+                if data['service'] in SERVICE_TYPES:
+                    carwash_income.service = data['service']
+                else:
+                    return make_response({'success': False, 'msg': f'service can only be one of {', '.join(SERVICE_TYPES)}'}, 400)
+                
+            if 'date' in data:
+                try:
+                    formatted_date = datetime.strftime(data['date'], '%d-%m-%Y, %H:%M')
+                    carwash_income.date = formatted_date
+                except ValueError:
+                    return make_response({'success': False, 'msg': 'invalid date format'}, 400)
+            
+            db.session.commit()
+            logger.info(f"carwash income entry {carwash_income.id} has been updated", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': True, 'msg': 'income entry updated successfully'}, 200)
+
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            logger.error(f"a database error occured trying to update a carwash income entry: {str(e)}", extra={'useer_id': get_jwt_identity()})
+            return make_response({'success': False, 'msg': 'failed to update entry, please try again'}, 400)
+        
+        except IntegrityError as e:
+            db.session.rollback()
+            logger.error(f"unique constraint violation trying to update carwash income: {str(e)}", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': False, 'msg': 'payment reference number already exists'}, 400)
+        
+    except Exception as e:
+        logger.error(f"an error occured trying to update carwash income entry: {str(e)}", extra={'user_id': get_jwt_identity()})
+        return make_response({'success': False, 'msg': 'internal server error'}, 500)
+    
+@carwash_bp.route('/carwash/income/<int:income_id>/delete', methods=['DELETE'])
+@jwt_required()
+def delete_carwash_income(income_id):
+    try:
+        carwash_income = CarwashIncome.query.get(income_id)
+        if not carwash_income:
+            return make_response({'success': False, 'msg': 'income record not found'}, 404)
+        
+        try:
+            db.session.delete(carwash_income)
+            db.session.commit()
+            
+            logger.info(f"carwash income record {income_id} deleted", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': True, 'msg': 'income entry deleted successfully'}, 200)
+        
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            logger.error(f"database error occured trying to delete a record of carwash income: {str(e)}", extra={'user_id': get_jwt_identity()})
+            return make_response({'success': False, 'msg': 'failed to delete carwash income record, please try again'}, 500)
+        
+    except Exception as e:
+        logger.error(f"an error occured trying to delete an income record: {str(e)}", extra={'user_id': get_jwt_identity()})
         return make_response({'success': False, 'msg': 'internal server error'}, 500)
